ea1770ddcfdfc1f161b6171f19ea3ea829586c01
playerthomasm6 <69253499+playerthomasm6@users.noreply.github.com>
2021-01-31 19:10:51 -0500

    added eat-da-burger

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,5 +1,6 @@
 import React from "react";
 import DefaultImage from "../Images/150x150.png"
+import EatDaBurger from "../Images/eat-da-burger.JPG"
 import Nav from "../components/Nav";
 import "./style.css";
 import Footer from"../components/Footer";
@@ -15,14 +16,13 @@ function Portfolio(props) {
     <article id="portfolio" class="row">
       
       <div class="col-sm-2">
-        <img id="image-border" src={DefaultImage} alt="Headshot" class="img-thumbnail headshot"/>
+        <img id="image-border" src={EatDaBurger} alt="Headshot" class="img-thumbnail headshot"/>
       </div>
 
       <div id="portfolio" class="col-sm-10 bg-dark">
-        <h3><a href="http://google.com">Project #1</a></h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad hic porro a adipisci
-          ipsa corporis omnis, animi excepturi nam, vero ullam architecto provident nisi fugiat
-          eum voluptatum quaerat voluptatibus laboriosam.
+        <h3><a href="https://calm-anchorage-67744.herokuapp.com/">Eat-Da-Burger</a></h3>
+        <p>Eat the burger is a simple app to allow a user to create burger names.  
+          The “burgers” can then be eaten and added to a new list.
         </p>
       </div>
     </article>
