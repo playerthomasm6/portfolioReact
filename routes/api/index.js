07eb4ab9b494d56035350bc444d4bc62af6982ba
playerthomasm6 <69253499+playerthomasm6@users.noreply.github.com>
2021-01-31 20:06:03 -0500

    fixed routing problem

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,8 @@
-// ===============================================================
-//         SAMPLE CODE FOR FUTURE DEVELOPMENT
-// ===============================================================
 
-// const router = require("express").Router();
-// const bookRoutes = require("./books");
+const router = require("express").Router();
+ const bookRoutes = require("./books");
 
-// // Book routes
-// router.use("/home", bookRoutes);
+ // Book routes
+ router.use("/home", bookRoutes);
 
-// module.exports = router;
+ module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,14 @@
-// ===============================================================
-//         SAMPLE CODE FOR FUTURE DEVELOPMENT
-// ===============================================================
 
-// const path = require("path");
-// const router = require("express").Router();
-// const apiRoutes = require("./api");
+ const path = require("path");
+const router = require("express").Router();
 
-// // API Routes
-// router.use("/api", apiRoutes);
 
-// // If no API routes are hit, send the React app
-// router.use(function(req, res) {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
+ // API Routes
+ 
 
-// module.exports = router;
+ // If no API routes are hit, send the React app
+ router.use(function(req, res) {
+   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+ });
+
+ module.exports = router;
