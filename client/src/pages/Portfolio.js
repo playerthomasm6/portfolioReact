10b559f38ea5a854cf7eabed1fd0ca5766f8afad
playerthomasm6 <69253499+playerthomasm6@users.noreply.github.com>
2021-01-31 21:10:33 -0500

    added portfolio pictures and links

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,6 +1,9 @@
 import React from "react";
 import DefaultImage from "../Images/150x150.png"
 import EatDaBurger from "../Images/eat-da-burger.JPG"
+import RadicalGames from "../Images/radicalGames.JPG"
+import NoteTaker from "../Images/noteTaker.JPG"
+import Tipster from "../Images/tipster.JPG"
 import Nav from "../components/Nav";
 import "./style.css";
 import Footer from"../components/Footer";
@@ -30,42 +33,36 @@ function Portfolio(props) {
     <article id="portfolio" class="row">
 
       <div class="col-sm-2">
-        <img id="image-border" src={DefaultImage} alt="Headshot" class="img-thumbnail headshot"/>
+        <img id="image-border" src={RadicalGames} alt="Headshot" class="img-thumbnail headshot"/>
       </div>
 
       <div id="portfolio" class="col-sm-10 bg-dark">
-        <h3><a href="http://google.com">Project #2</a></h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad hic porro a adipisci
-          ipsa corporis omnis, animi excepturi nam, vero ullam architecto provident nisi fugiat
-          eum voluptatum quaerat voluptatibus laboriosam.
+        <h3><a href="https://raticles.herokuapp.com/index.html">Radical Gamez</a></h3>
+        <p>Radical Games is a web app which allows a user to search games and add them to a wish list.
         </p>
       </div>
     </article>
 
     <article id="portfolio" class="row">
       <div class="col-sm-2">
-        <img id="image-border" src={DefaultImage} alt="Headshot" class="img-thumbnail headshot"/>
+        <img id="image-border" src={NoteTaker} alt="Headshot" class="img-thumbnail headshot"/>
       </div>
 
       <div id="portfolio" class="col-sm-10 bg-dark">
-        <h3><a href="http://google.com">Project #3</a></h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad hic porro a adipisci
-          ipsa corporis omnis, animi excepturi nam, vero ullam architecto provident nisi fugiat
-          eum voluptatum quaerat voluptatibus laboriosam.
+        <h3><a href="https://limitless-reaches-25124.herokuapp.com/notes">Note Taker App</a></h3>
+        <p>This app allows a user to create notes and save them to view or edit later.
         </p>
       </div>
     </article>
 
     <article id="portfolio" class="row">
       <div class="col-sm-2">
-        <img id="image-border" src={DefaultImage} alt="Headshot" class="img-thumbnail headshot"/>
+        <img id="image-border" src={Tipster} alt="Headshot" class="img-thumbnail headshot"/>
       </div>
 
       <div id="portfolio" class="col-sm-10 bg-dark">
-        <h3><a href="http://google.com">Project #4</a></h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad hic porro a adipisci
-          ipsa corporis omnis, animi excepturi nam, vero ullam architecto provident nisi fugiat
-          eum voluptatum quaerat voluptatibus laboriosam.
+        <h3><a href="https://playerthomasm6.github.io/tipster/">Tipster</a></h3>
+        <p>This app allows the user to search any movie or television show and using an algorithm pairs a beer with the movie.
         </p>
       </div>
     </article>
