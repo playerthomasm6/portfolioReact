33e23456ca8d89f810cd5a6be76cd1000072c12a
playerthomasm6 <69253499+playerthomasm6@users.noreply.github.com>
2021-01-31 19:21:57 -0500

    run build for deployment

---
+++ client/build/precache-manifest.592180af51704bd6fecbfe4330f36f78.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c8efa6e53ddf899d71b6ec2335ef212",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1c701210c2b1d84b53c",
+    "url": "/static/css/main.8eefefba.chunk.css"
+  },
+  {
+    "revision": "0ad7755ab68fc53231cb",
+    "url": "/static/js/2.4b66e6ac.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.4b66e6ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1c701210c2b1d84b53c",
+    "url": "/static/js/main.90bbff16.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "2bac5af653cc028a2c8454f25e774b44",
+    "url": "/static/media/Bg-02.2bac5af6.jpg"
+  },
+  {
+    "revision": "6600f1f573c384e844792a01d13e72a7",
+    "url": "/static/media/Bl-logo.6600f1f5.png"
+  },
+  {
+    "revision": "75b482e426af553c4f23968731f1f277",
+    "url": "/static/media/Family-Dogs.75b482e4.jpg"
+  },
+  {
+    "revision": "16d3696bb75aeb57e3da750ca97a9d81",
+    "url": "/static/media/Headshot-01.16d3696b.jpg"
+  },
+  {
+    "revision": "cc678b77808138ca372fd2b77185b9ce",
+    "url": "/static/media/eat-da-burger.cc678b77.JPG"
+  }
+]);
