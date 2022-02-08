b34156e96cb0c20c0e8de55190d64f20cd60ef27
Thomas Player <playerthomasm6@gmail.com>
2022-02-08 10:16:09 -0500

    added new logo

--- client/src/pages/Graphic.js
+++ client/src/pages/Graphic.js
@@ -8,38 +8,54 @@ import PlaceHolder150x150 from "../Images/150x150.png";
 import CHF_Logo from "../Images/CHF_Logo.PNG";
 import NJS_Logo from "../Images/NJsoldiers_logo.png";
 import Northstar_logo from "../Images/Northstar STAR logo version2.png"
+import Rocket_Logo_Outline from "../Images/Rocket-OutLine.png"
+import Rocket_Logo from "../Images/Rocket-No Line.png"
 
 function Graphic() {
 
   const graphicData = [ // Array which holds graphic design data to display
     {
       key: "A1",
+      title: "3D Planet Explorer",
+      imageSource: Rocket_Logo_Outline,
+      imageAlt: "Rocket with outline",
+      descriptionText: "This logo graphic was created for the 3D Planet Explorer website I created.  You can find a link to this web site on the Web Development tab."
+    },
+    {
+      key: "A2",
+      title: "3D Planet Explorer",
+      imageSource: Rocket_Logo,
+      imageAlt: "Rocket without outline",
+      descriptionText: "This logo graphic was created for the 3D Planet Explorer website I created.  You can find a link to this web site on the Web Development tab."
+    },
+    {
+      key: "A3",
       title: "Northstar Marine Inc",
       imageSource: Northstar_logo,
       imageAlt: "Northstar Marine Inc Logo",
       descriptionText: "This logo was created for Northstar to promote their new safety program.  This logo will be featured on merchandise as well as internally for other safety related material."
     },
     {
-      key: "A2",
+      key: "A4",
       title: "Court House Fitness",
       imageSource: CHF_Logo,
       imageAlt: "Court House Fitness Logo",
       descriptionText: "This graphic was designed for t-shirts to be sold by Court House Fitness"
     },
     {
-      key: "A3",
+      key: "A5",
       title: "New Jersey Soldiers",
       imageSource: NJS_Logo,
       imageAlt: "New Jersey Soldiers logo",
       descriptionText: "This graphic was a reskin and recolor of a previous logo design.  The image was created for the purpose of social media posts and emailing."
     },
-    {
-      key: "A4",
-      title: "PlaceHolder",
-      imageSource: PlaceHolder150x150,
-      imageAlt: "Placeholder Image",
-      descriptionText: "This is placeholder text until I design more graphics professionally"
-    }
+    // {
+    //   key: "A6",
+    //   title: "PlaceHolder",
+    //   imageSource: PlaceHolder150x150,
+    //   imageAlt: "Placeholder Image",
+    //   descriptionText: "This is placeholder text until I design more graphics professionally"
+    // }
   ];
 
   return (
@@ -48,7 +64,7 @@ function Graphic() {
     <div className="container-fluid">
       <div className="row">
           <div className="col-12 title-text">
-            <h1>Some of my graphic logo work</h1>
+            <h1>My graphic design portfolio</h1>
           </div>
       </div>
       
